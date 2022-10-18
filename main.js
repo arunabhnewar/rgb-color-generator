eaa88593e5b80888b06b8c913b61fa80adc82f3c
arunabhnewar <newardevil@gmail.com>
2022-10-18 12:03:14 +0600

    second commit

--- main.js
+++ main.js
@@ -9,24 +9,27 @@ function myBgColor() {
     // Step 3 - Collect all necessary reference
     const content = document.getElementById('content');
     const btn = document.getElementById('change_btn')
-
+    const output = document.getElementById('output')
 
     // Step 4 - Handle the click event
     btn.addEventListener('click', function () {
         const rgbBgColor = generateRGBColor();
         content.style.background = rgbBgColor;
+        output.value = rgbBgColor;
     })
+
+    // Step 2 - Random color generator function
+    function generateRGBColor() {
+        const red = Math.floor(Math.random() * 255);
+        const green = Math.floor(Math.random() * 255);
+        const blue = Math.floor(Math.random() * 255);
+
+        return `rgb(${red}, ${green}, ${blue})`
+    }
+
 }
 
 
-// Step 2 - Random color generator function
-function generateRGBColor() {
-    const red = Math.floor(Math.random() * 255);
-    const green = Math.floor(Math.random() * 255);
-    const blue = Math.floor(Math.random() * 255);
-
-    return `rgb(${red}, ${green}, ${blue})`
-}
 
 
 
